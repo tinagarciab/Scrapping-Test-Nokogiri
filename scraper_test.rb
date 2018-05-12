a6287e3b638cc79a35632281b024189738740f27
Valentina Garcia Bustamante <you@example.com>
2018-05-12 19:23:06 -0400

    second commit

---
+++ scraper_test.rb
@@ -0,0 +1,14 @@
+# Test Based on Nokogiri Tutorial 
+
+require 'HTTParty'
+require 'Nokogiri'
+require 'Pry'
+require 'csv'
+
+#Request the page tahts ig going to scrape
+page = HTTParty.get('https://www.kijiji.ca/b-maison-a-louer/grand-montreal/c43l80002?siteLocale=fr_CA')
+
+
+
+
+
