565bee66461e36eab9df7c2e649d229c1e82f47d
Valentina Garcia Bustamante <you@example.com>
2018-05-12 19:35:16 -0400

    Test 1

--- scraper_test.rb
+++ scraper_test.rb
@@ -8,7 +8,11 @@ require 'csv'
 #Request the page tahts ig going to scrape
 page = HTTParty.get('https://www.kijiji.ca/b-maison-a-louer/grand-montreal/c43l80002?siteLocale=fr_CA')
 
+#extract the HTML information of the page 
+File.open("page.html", 'w') { |file| file.write(page) }
 
 
+#Transform http response into a nokogiri object to parse it
+parse_page= Nokogiri::HTML(page)
 
-
+Pry.start(binding)
