5b46c1b962c48359baeba0b48fdcdb59368cfb9d
Valentina Garcia Bustamante <you@example.com>
2018-05-12 20:24:22 -0400

    Test 2

--- scraper_test.rb
+++ scraper_test.rb
@@ -15,4 +15,19 @@ File.open("page.html", 'w') { |file| file.write(page) }
 #Transform http response into a nokogiri object to parse it
 parse_page= Nokogiri::HTML(page)
 
-Pry.start(binding)
+# Empty array where will be store all the craigslist Rent listing for Montreal 
+rent_array = []
+
+
+#Here is where de data is parsing
+parse_page.css('.info').css('.title').map do |a|
+	post_name = a.text
+	rent_array.push(post_name)
+end
+
+# this will push your array into a CSV file
+CSV.open('rent.csv', 'w') do |csv|
+	csv << rent_array
+end
+
+Pry.start(binding)	
